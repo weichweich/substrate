35e522acd3e5ae48775a21d50fe7cea534795fa8
Alexander Theißen <alexander.theissen@parity.io>
2020-06-29 12:24:12 +0200

    Fix wasm tests

--- frame/contracts/src/wasm/mod.rs
+++ frame/contracts/src/wasm/mod.rs
@@ -1641,55 +1641,13 @@ mod tests {
 		).unwrap();
 	}
 
-	// asserts that the size of the input data is 4.
-	const CODE_SIMPLE_ASSERT: &str = r#"
-(module
-	(import "env" "ext_scratch_size" (func $ext_scratch_size (result i32)))
-
-	(func $assert (param i32)
-		(block $ok
-			(br_if $ok
-				(get_local 0)
-			)
-			(unreachable)
-		)
-	)
-
-	(func (export "deploy"))
-
-	(func (export "call")
-		(call $assert
-			(i32.eq
-				(call $ext_scratch_size)
-				(i32.const 4)
-			)
-		)
-	)
-)
-"#;
-
-	#[test]
-	fn output_buffer_capacity_preserved_on_success() {
-		let mut input_data = Vec::with_capacity(1_234);
-		input_data.extend_from_slice(&[1, 2, 3, 4][..]);
-
-		let output = execute(
-			CODE_SIMPLE_ASSERT,
-			input_data,
-			MockExt::default(),
-			&mut GasMeter::new(GAS_LIMIT),
-		).unwrap();
-
-		assert_eq!(output.data.len(), 0);
-		assert_eq!(output.data.capacity(), 1_234);
-	}
-
 	const CODE_RETURN_WITH_DATA: &str = r#"
 (module
-	(import "env" "ext_scratch_size" (func $ext_scratch_size (result i32)))
-	(import "env" "ext_scratch_read" (func $ext_scratch_read (param i32 i32 i32)))
-	(import "env" "ext_scratch_write" (func $ext_scratch_write (param i32 i32)))
+	(import "env" "ext_input" (func $ext_input (param i32 i32)))
+	(import "env" "ext_return" (func $ext_return (param i32 i32)))
 	(import "env" "memory" (memory 1 1))
+
+	(data (i32.const 32) "\20")
 
 	;; Deploy routine is the same as call.
 	(func (export "deploy") (result i32)
@@ -1701,33 +1659,24 @@ mod tests {
 		(local $buf_size i32)
 		(local $exit_status i32)
 
-		;; Find out the size of the scratch buffer
-		(set_local $buf_size (call $ext_scratch_size))
-
-		;; Copy scratch buffer into this contract memory.
-		(call $ext_scratch_read
-			(i32.const 0)		;; The pointer where to store the scratch buffer contents,
-			(i32.const 0)		;; Offset from the start of the scratch buffer.
-			(get_local $buf_size)		;; Count of bytes to copy.
+		;; Copy input data this contract memory.
+		(call $ext_input
+			(i32.const 0)	;; Pointer where to store input
+			(i32.const 32)	;; Pointer to the length of the buffer
 		)
 
 		;; Copy all but the first 4 bytes of the input data as the output data.
-		(call $ext_scratch_write
-			(i32.const 4)		;; Offset from the start of the scratch buffer.
-			(i32.sub		;; Count of bytes to copy.
-				(get_local $buf_size)
-				(i32.const 4)
-			)
+		(call $ext_return
+			(i32.const 4)
+			(i32.sub (i32.load (i32.const 32)) (i32.const 4))
 		)
-
-		;; Return the first 4 bytes of the input data as the exit status.
-		(i32.load (i32.const 0))
+		(unreachable)
 	)
 )
 "#;
 
 	#[test]
-	fn return_with_success_status() {
+	fn ext_return() {
 		let output = execute(
 			CODE_RETURN_WITH_DATA,
 			hex!("00112233445566778899").to_vec(),
@@ -1737,18 +1686,5 @@ mod tests {
 
 		assert_eq!(output, ExecReturnValue { status: 0, data: hex!("445566778899").to_vec() });
 		assert!(output.is_success());
-	}
-
-	#[test]
-	fn return_with_failure_status() {
-		let output = execute(
-			CODE_RETURN_WITH_DATA,
-			hex!("112233445566778899").to_vec(),
-			MockExt::default(),
-			&mut GasMeter::new(GAS_LIMIT),
-		).unwrap();
-
-		assert_eq!(output, ExecReturnValue { status: 17, data: hex!("5566778899").to_vec() });
-		assert!(!output.is_success());
 	}
 }
